278b4ff197665840dd6b93f6b202251f7a3ff9d2
vayudarji <vayutailor@gmail.com>
2024-09-22 22:12:21 +0530

    Auth Controller

---
+++ src/main/java/com/hotelbooking/HotelBooking/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.hotelbooking.HotelBooking.controller;
+
+import com.hotelbooking.HotelBooking.dto.LoginRequest;
+import com.hotelbooking.HotelBooking.dto.Response;
+import com.hotelbooking.HotelBooking.entity.User;
+import com.hotelbooking.HotelBooking.service.interfac.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
